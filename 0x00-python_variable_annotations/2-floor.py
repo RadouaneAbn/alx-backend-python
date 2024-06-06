3fd7d6d2b064561f548136631cd42bd6f43f2050
RadouaneAbn <radouaneabn@gmail.com>
2024-06-06 23:08:49 +0100

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 2-floor.py """
+
+
+def floor(n: float) -> int:
+    """
+    Calculates the floor of a number.
+    Args:
+        n (int): The float number.
+
+    Returns: the floor of a float.
+    """
+    return int(n)
