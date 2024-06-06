b69a295b3f270313743d7294d7dc248ba69fd000
RadouaneAbn <radouaneabn@gmail.com>
2024-06-06 23:33:20 +0100

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 3-to_str.py """
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to string
+    Args:
+        n (int): the float number
+
+    Returns: a string representaion of the number
+    """
+    return str(n)
