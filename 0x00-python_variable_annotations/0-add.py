51323b0829229d4ad1f22b8eadbbfc3f67f636ff
RadouaneAbn <radouaneabn@gmail.com>
2024-06-06 22:46:13 +0100

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" 0-add """
+
+def add(a: float, b: float) -> float:
+    """
+    Add two numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The result of the sum.
+    """
+    return a + b
