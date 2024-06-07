d69e61b30d2d5bbfcedc99883b395d3f29b58c65
RadouaneAbn <radouaneabn@gmail.com>
2024-06-07 01:11:05 +0100

    Task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" 8-make_multiplier.py """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable:
+    """
+    makes a funtion that calculates the multiplication of
+    multiplier and a float
+
+    Args:
+        multiplier (float): The multiplier
+
+    Returns: a function that calculates the multiplication of
+        multiplier and a float
+    """
+    def multiply(n: float) -> float:
+        """
+        Calculates the multiplication of multiplier and a float
+
+        Args:
+            n (float): The given float
+
+        Returns: The result of teh multiplication
+        """
+        return n * multiplier
+    return multiply
