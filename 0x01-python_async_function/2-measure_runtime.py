29a9e48b8cc20abf7e42b1ec3f1c5727b3a908eb
RadouaneAbn <radouaneabn@gmail.com>
2024-06-12 00:32:17 +0100

    Task 2 - annotation

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
     totale_time = time.time() - start
