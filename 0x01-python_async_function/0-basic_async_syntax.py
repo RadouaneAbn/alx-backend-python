45533dc0d42c10b8890637cd96dadc9d0da4b87c
RadouaneAbn <radouaneabn@gmail.com>
2024-06-11 23:47:01 +0100

    Task 0 - change annotion

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ 0-basic_async_syntax.py """
-from typing import Union
+
 import random
 from asyncio import sleep
 
 
-async def wait_random(max_delay: Union[int, float]=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ This function returns a  """
     wait_time = random.uniform(0, max_delay)
     await sleep(wait_time)
