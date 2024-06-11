a11b1ac274c7c03a35866a46944b21374f204b3f
RadouaneAbn <radouaneabn@gmail.com>
2024-06-12 00:19:23 +0100

    Task 1 - fix annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """ 1-concurrent_coroutines.py """
 
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ This function execute wait_random for n times
         and append the wait time to a list when it finishes
         executing each time
