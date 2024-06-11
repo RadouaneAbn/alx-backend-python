750ef722ea00d38de66836542ad7acf92741606a
RadouaneAbn <radouaneabn@gmail.com>
2024-06-11 23:39:03 +0100

    Task 0 - check 3 'red'

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ 0-basic_async_syntax.py """
 import random
-from time import sleep
+from asyncio import sleep
 
 
 async def wait_random(max_delay=10):
     """ This function returns a  """
     wait_time = random.uniform(0, max_delay)
-    sleep(wait_time)
+    await sleep(wait_time)
     return wait_time
