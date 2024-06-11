31adb860b8db9a187660f5e681d06937969763d6
RadouaneAbn <radouaneabn@gmail.com>
2024-06-12 00:56:33 +0100

    use task_wait_random

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,18 +4,16 @@
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int):
-    async def wait_n(n: int, max_delay: int) -> List[float]:
-        """ This function return a task that exexute wait_random for n times
-            and append the wait time to a list when it finishes
-            executing each time
-        """
-        tasks = [wait_random(max_delay) for _ in range(n)]
-        delays = []
-        for task in asyncio.as_completed(tasks):
-            delays.append(await task)
-        return delays
-    return await asyncio.create_task(wait_n(n, max_delay))
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ This function execute task_wait_random for n times
+        and append the wait time to a list when it finishes
+        executing each time
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for task in asyncio.as_completed(tasks):
+        delays.append(await task)
+    return delays
