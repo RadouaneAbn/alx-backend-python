16bcb5aa664f04a0e236c6f7f986d34b8731e6bf
RadouaneAbn <radouaneabn@gmail.com>
2024-06-12 01:50:52 +0100

    Task 2 - annotaion + document module

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" 2-measure_runtime.py """
 
 import asyncio
 from time import time
@@ -6,7 +7,7 @@ from time import time
 async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
         This function returns the time needed to execute all tasks
         in parallel.
