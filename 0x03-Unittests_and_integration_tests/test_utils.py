2330b2d1d02974ca6b92d89598a96bbbcd27ef12
RadouaneAbn <radouaneabn@gmail.com>
2024-07-29 17:00:28 +0100

    Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ This module contains unit tests for a access_nested_map function """
 import unittest
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
 access_nested_map = __import__("utils").access_nested_map
+get_json = __import__("utils").get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,3 +51,33 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(context.exception.args[0], expected)
+
+
+class TEstGetJson(unittest.TestCase):
+    """
+        This class is a mock unittest for get_json function.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+            Test get_json function returns the correct pyload and ensure the
+            get method is called only once per url.
+
+            Args:
+                test_url (str): the URL.
+                test_payload (dict): the returned dict.
+                mock_get (MagickMock): the mocked requests.get method.
+
+            Asserts:
+                The payload returned by the mocked requests.
+                The requests.get method was called exactly once per URL.
+        """
+        mock_res = MagicMock(status_code=200)
+        mock_res.json.return_value = test_payload
+        mock_get.return_value = mock_res
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get.assert_called_once_with(test_url)
